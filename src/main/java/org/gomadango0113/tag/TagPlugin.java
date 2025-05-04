3bddcc63ea4180b93983ecf30a1a3ff6795d5b76
gomadango0113 <gomadango0113@gmail.com>
2025-05-04 20:43:31 +0900

    first commit

---
+++ src/main/java/org/gomadango0113/tag/TagPlugin.java
@@ -0,0 +1,17 @@
+package org.gomadango0113.tag;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TagPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
