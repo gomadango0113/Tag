e243b0ec02c210d92a58fe40fc8abce10d2cac7d
gomadango0113 <gomadango0113@gmail.com>
2025-05-09 20:23:35 +0900

    【追加】ゲームを開始できるように追加

--- src/main/java/org/gomadango0113/tag/command/GameStartCommand.java
+++ src/main/java/org/gomadango0113/tag/command/GameStartCommand.java
@@ -3,13 +3,19 @@ package org.gomadango0113.tag.command;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.gomadango0113.tag.manager.GameManager;
+import org.gomadango0113.tag.util.ChatUtil;
 
 public class GameStartCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender send, Command cmd, String s, String[] args) {
         if (cmd.getName().equalsIgnoreCase("tag_start")) {
-
+            int game = GameManager.startGame();
+            ChatUtil.sendMessage(send, "ゲームを開始しています...");
+            if (game == 1) {
+                ChatUtil.sendMessage(send, "ゲームを開始することができませんでした。");
+            }
         }
         return false;
     }
---
+++ src/main/java/org/gomadango0113/tag/manager/GameManager.java
@@ -0,0 +1,62 @@
+package org.gomadango0113.tag.manager;
+
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+import org.gomadango0113.tag.TagPlugin;
+import org.gomadango0113.tag.util.ChatUtil;
+
+public class GameManager {
+
+    private static GameStatus status;
+    private static int GAME_TIME;
+    private static BukkitTask task;
+
+    static {
+        status = GameStatus.WAITING;
+        GAME_TIME = 60*15;
+    }
+
+    public static int startGame() {
+        if (status == GameStatus.WAITING && task == null) {
+            final int[] count_time = {10};
+
+            task = new BukkitRunnable() {
+                @Override
+                public void run() {
+                    if (status == GameStatus.WAITING || status == GameStatus.COUNTING) {
+                        if (count_time[0] == 0) {
+                            ChatUtil.sendGlobalMessage("ゲーム開始!!");
+                            status = GameStatus.RUNNING;
+                        }
+                        else {
+                            status = GameStatus.COUNTING;
+                            String count_format = String.format("ゲーム開始まであと%d秒", count_time[0]);
+                            ChatUtil.sendGlobalMessage(count_format);
+                            count_time[0]--;
+                        }
+                    }
+                    else if (status == GameStatus.RUNNING) {
+
+                        if (GAME_TIME == 0) {
+                            ChatUtil.sendGlobalMessage("ゲーム終了!!");
+                            this.cancel();
+                        }
+
+                        GAME_TIME--;
+                    }
+                }
+            }.runTaskTimer(TagPlugin.getInstance(), 0L, 20L);
+
+            return 0;
+        }
+
+        return 1;
+    }
+
+    public enum GameStatus {
+        WAITING,
+        RUNNING,
+        COUNTING,
+        ENDING,
+    }
+}
